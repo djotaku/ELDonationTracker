e7bbafc9fe1174e56f3e9daada2ebf563765920a
Eric Mesa <ericsbinaryworld@gmail.com>
2016-10-06 20:54:12 -0400

    Changed it from a script that just runs straight through to being based on functions. This allows it to be used in a GUI in the future

--- extralifedonations.py
+++ extralifedonations.py
@@ -14,70 +14,74 @@ donors="http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participantDon
 team="http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&teamID="+str(TeamID)+"&format=json"
 teamroster="http://www.extra-life.org/index.cfm?fuseaction=donorDrive.teamParticipants&teamID="+str(TeamID)+"&format=json"
 
-#this part needs to be in a loop
-while True:
-    #Get the Info
-    participantJSON=json.load(urllib2.urlopen(participant))
-    donorJSON=json.load(urllib2.urlopen(donors))
-    if TeamID != None:
-        teamJSON=json.load(urllib2.urlopen(team))
-        teamrosterJSON=json.load(urllib2.urlopen(teamroster))
-
-    #fordebug
-    #print str(participantJSON)+"\n\n"
-    #print str(donorJSON)+"\n\n"
-    print (time.strftime("%H:%M:%S"))
-
-    totalRaised=CurrencySymbol+'{:.2f}'.format(participantJSON['totalRaisedAmount'])
-    f = open(textFolder+'totalRaised.txt', 'w')
-    f.write(totalRaised)
+def writetofile(info,filename):
+    "Handles all the file writes"
+    f = open(textFolder+filename, 'w')
+    f.write(info)
     f.close
 
-    goal=CurrencySymbol+'{:.2f}'.format(participantJSON['fundraisingGoal'])
-    f = open(textFolder+'goal.txt', 'w')
-    f.write(goal)
-    f.close
 
-    LastDonorNameAmnt=str(donorJSON[0]['donorName'])+" - "+CurrencySymbol+'{:.2f}'.format(donorJSON[0]['donationAmount'])
-    f = open(textFolder+'LastDonorNameAmnt.txt', 'w')
-    f.write(LastDonorNameAmnt)
-    f.close
+def ParticpantTotalRaised(JSON):
+    totalRaised=CurrencySymbol+'{:.2f}'.format(JSON['totalRaisedAmount'])
+    writetofile(totalRaised,"totalRaised.txt")
 
-    #for finding top donor
+def ParticipantGoal(JSON):
+    goal=CurrencySymbol+'{:.2f}'.format(JSON['fundraisingGoal'])
+    writetofile(goal,"goal.txt")
+    
+def ParticipantLastDonorNameAmnt(JSON):
+    LastDonorNameAmnt=str(JSON[0]['donorName'])+" - "+CurrencySymbol+'{:.2f}'.format(JSON[0]['donationAmount'])
+    writetofile(LastDonorNameAmnt,"LastDonorNameAmnt.txt")
+
+def ParticipantTopDonor(JSON):
     TopDonorIndex=0
     TopDonorNameAmnt=""
-    for donor in range(0,len(donorJSON)):
+    for donor in range(0,len(JSON)):
         #need to deal with donations where they hid the donationAmount
-        if donorJSON[donor]['donationAmount'] == None:
+        if JSON[donor]['donationAmount'] == None:
             print "skipping a null donation amount"
-        elif int(donorJSON[donor]['donationAmount'])>int(donorJSON[TopDonorIndex]['donationAmount']):
+        elif int(JSON[donor]['donationAmount'])>int(JSON[TopDonorIndex]['donationAmount']):
             TopDonorIndex=donor
-        TopDonorNameAmnt=str(donorJSON[TopDonorIndex]['donorName'])+" - "+CurrencySymbol+'{:.2f}'.format(donorJSON[TopDonorIndex]['donationAmount'])
-    f = open(textFolder+'TopDonorNameAmnt.txt', 'w')
-    f.write(TopDonorNameAmnt)
-    f.close
+        TopDonorNameAmnt=str(JSON[TopDonorIndex]['donorName'])+" - "+CurrencySymbol+'{:.2f}'.format(JSON[TopDonorIndex]['donationAmount'])
+    writetofile(TopDonorNameAmnt,"TopDonorNameAmnt.txt")
 
-
+def Participantlast5DonorNameAmts(JSON):
     last5DonorNameAmts=""
-    for donor in range(0, len(donorJSON)):
-        last5DonorNameAmts=last5DonorNameAmts+str(donorJSON[donor]['donorName'])+" - "+CurrencySymbol+str(donorJSON[donor]['donationAmount'])+"0\n"
+    for donor in range(0, len(JSON)):
+        last5DonorNameAmts=last5DonorNameAmts+str(JSON[donor]['donorName'])+" - "+CurrencySymbol+str(JSON[donor]['donationAmount'])+"0\n"
         if donor==4:
             break
-    f = open(textFolder+'last5DonorNameAmts.txt', 'w')
-    f.write(last5DonorNameAmts)
-    f.close
+    writetofile(last5DonorNameAmts,"last5DonorNameAmts.txt")
+
+def TheTeamGoal(JSON):
+    TeamGoal=CurrencySymbol+'{:.2f}'.format(JSON['fundraisingGoal'])
+    writetofile(TeamGoal,"TeamGoal.txt")
     
-    #team stuff
-    if TeamID != None:
-        TeamGoal=CurrencySymbol+'{:.2f}'.format(teamJSON['fundraisingGoal'])
-        f = open(textFolder+'TeamGoal.txt', 'w')
-        f.write(TeamGoal)
-        f.close
+def TheTeamTotalRaised(JSON):
+    TeamTotalRaised=CurrencySymbol+'{:.2f}'.format(JSON['totalRaisedAmount'])
+    writetofile(TeamTotalRaised,"TeamTotalRaised.txt")
+#this part needs to be in a loop
+
+if __name__=="__main__":
+    print "It's GO TIME!"
+    while True:
         
-        TeamTotalRaised=CurrencySymbol+'{:.2f}'.format(teamJSON['totalRaisedAmount'])
-        f = open(textFolder+'TeamTotalRaised.txt', 'w')
-        f.write(TeamTotalRaised)
-        f.close
-    
-    time.sleep(120)
-#end loop
+        #participant stuff
+        participantJSON=json.load(urllib2.urlopen(participant))
+        donorJSON=json.load(urllib2.urlopen(donors))
+
+        print (time.strftime("%H:%M:%S"))
+        
+        ParticpantTotalRaised(participantJSON)
+        ParticipantGoal(participantJSON)
+        ParticipantLastDonorNameAmnt(donorJSON)
+        ParticipantTopDonor(donorJSON)
+        Participantlast5DonorNameAmts(donorJSON)
+        
+        if TeamID != None:
+            teamJSON=json.load(urllib2.urlopen(team))
+            teamrosterJSON=json.load(urllib2.urlopen(teamroster))
+            TheTeamGoal(teamJSON)
+            TheTeamTotalRaised(teamJSON)
+        
+        time.sleep(120)
