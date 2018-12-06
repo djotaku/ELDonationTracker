070b75a4e5e0aec3848f6c0e741c8844026a2b86
Eric Mesa <ericsbinaryworld@gmail.com>
2018-12-05 21:05:00 -0500

    changes - adds a GUI! Using QT Designer and pyuic5 design.ui -o design.py it's now super easy for me to design a GUI. I've made an example for myself of how to update with text at the time it's launched. Still TODO - add all the fields I've got in the webpage, options, and the tracker

---
+++ design.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'design.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(120, 10, 241, 41))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(370, 10, 261, 41))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(130, 90, 501, 192))
+        self.textBrowser.setDocumentTitle("")
+        self.textBrowser.setObjectName("textBrowser")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(140, 70, 121, 16))
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Extra Life Donation Tracker"))
+        self.pushButton.setText(_translate("MainWindow", "Options"))
+        self.pushButton_2.setText(_translate("MainWindow", "Tracker"))
+        self.textBrowser.setPlaceholderText(_translate("MainWindow", "Needs text"))
+        self.label.setText(_translate("MainWindow", "Recent Donations"))
+
---
+++ gui.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+import design, sys
+
+class ExampleApp(QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        # Explaining super is out of the scope of this article
+        # So please google it if you're not familar with it
+        # Simple reason why we use it here is that it allows us to
+        # access variables, methods etc in the design.py file
+        super(self.__class__, self).__init__()
+        self.setupUi(self)  # This is defined in design.py file automatically
+                            # It sets up layout and widgets that are defined
+        self.getsomeText()
+
+    def getsomeText(self):
+        f = open('/home/ermesa/Dropbox/ELtracker/last5DonorNameAmtsMessage.txt', 'r')
+        text=f.read()
+        self.textBrowser.setPlainText(text)
+        f.close()
+
+def main():
+    app = QApplication(sys.argv)  # A new instance of QApplication
+    form = ExampleApp()                 # We set the form to be our ExampleApp (design)
+    form.show()                         # Show the form
+    app.exec_()                         # and execute the app
+
+
+if __name__ == '__main__':              # if we're running file directly and not importing it
+    main()                              # run the main function
