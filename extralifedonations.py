42768c33b6f4b5fb15d566da8d0c8bcf33de3181
Eric Mesa <ericsbinaryworld@gmail.com>
2019-01-12 20:02:58 -0500

    just about done now with my first draft. Just need to actually get the write_text_files to write files rather than print to the screen. Then it's on to unit testing because not having any donations means I've certainly introducted a bug or two to a part of the code that isn't currently running.

--- extralifedonations.py
+++ extralifedonations.py
@@ -5,6 +5,8 @@ import urllib.request
 import time
 import unicodedata
 
+#api info at https://github.com/DonorDrive/PublicAPI
+
 class Donor:
     "This class exists to provide attributes for a donor based on what comes in from the JSON so that it doesn't have to be traversed each time a donor action needs to be taken"
     def __init__(self, name, message, amount):
@@ -28,7 +30,13 @@ class Participant:
         #if need to test with donations until write unit tests: 
         #self.donorURL = f"http://www.extra-life.org/api/participants/297674/donations"
         self.teamURL = f"http://www.extra-life.org/api/teams/{self.TeamID}"
-        
+        self.donorcalcs = {}
+        self.donorcalcs['LastDonorNameAmnt'] = "No Donors Yet"
+        self.donorcalcs['TopDonorNameAmnt'] = "No Donors Yet"
+        self.donorcalcs['last5DonorNameAmts'] = "No Donors Yet"
+        self.donorcalcs['last5DonorNameAmtsMessage'] = "No Donors Yet"
+        self.donorcalcs['last5DonorNameAmtsMessageHorizontal'] = "No Donors Yet"
+        self.participantinfo = {}
     
     def get_participant_JSON(self):
         """Connects to the server and grabs the participant JSON and populates info.
@@ -48,7 +56,10 @@ class Participant:
         self.participantgoal = self.participantJSON['fundraisingGoal']
         
         #the dictionary:
-        self.participantinfo = {'totalRaised':self.CurrencySymbol+'{:.2f}'.format(self.participantJSON['sumDonations']), "numDonations":str(self.participantJSON['numDonations']),"averageDonation":self.CurrencySymbol+'{:.2f}'.format(self.averagedonation), "goal":self.CurrencySymbol+'{:.2f}'.format(self.participantgoal)}
+        self.participantinfo['totalRaised'] =self.CurrencySymbol+'{:.2f}'.format(self.participantJSON['sumDonations'])
+        self.participantinfo["numDonations"] = str(self.participantJSON['numDonations'])
+        self.participantinfo["averageDonation"] = self.CurrencySymbol+'{:.2f}'.format(self.averagedonation)
+        self.participantinfo["goal"] = self.CurrencySymbol+'{:.2f}'.format(self.participantgoal)
     
     
     def get_donors(self):
@@ -92,78 +103,52 @@ class Participant:
     
     
     def _donor_calculations(self):
-        self.donorcalcs = {}
         self.donorcalcs['LastDonorNameAmnt'] = self._donor_formatting(self.donorlist[0],False)
-        self.donorcalcs['TopDonorNameAmnt'] = self._donor_formatting(sorted(self.donorlist,reverse=True),False)
+        self.donorcalcs['TopDonorNameAmnt'] = self._donor_formatting(sorted(self.donorlist,reverse=True)[0],False)
         self.donorcalcs['last5DonorNameAmts'] = self._last5donors(self.donorlist,False,False)
         self.donorcalcs['last5DonorNameAmtsMessage'] = self._last5donors(self.donorlist,True,False)
         self.donorcalcs['last5DonorNameAmtsMessageHorizontal'] = self._last5donors(self.donorlist,True,True)
     
-    def write_text_files(self):
-        #in order to make this work best, probably need to instantiate the donor and participant dictionaries (will need to redo syntax on participant dictionary) to be able to write blanks to the text files in the loop and thus create the files even if there are no donors. For donorcalcs maybe instead of blanks, prepopulate with "No donors yet"
-        pass
+    def write_text_files(self,dictionary):
+        """description"""
+        for filename, text in dictionary.items():
+            print (filename,text)
     
     def run(self):
         "This should run getParticipantJSON, getDonors, the calculations methnods, and the methods to write to text files"
         self.get_participant_JSON()
         NumberofDonors = self.ParticipantNumDonations
+        self.write_text_files(self.participantinfo)
         self.get_donors()
         if self.donorlist:
             self._donor_calculations()
+            self.write_text_files(self.donorcalcs)
         while True:
             self.get_participant_JSON
+            self.write_text_files(self.participantinfo)
             if self.ParticipantNumDonations > NumberofDonors:
                 print("A new donor!")
                 NumberofDonors = self.ParticipantNumDonations
                 self.get_donors()
                 self._donor_calculations()
+                self.write_text_files(self.donorcalcs)
+            print (time.strftime("%H:%M:%S"))
             time.sleep(30)
                 
 
 
 ########### OLD Non-Class Way ######################
-#variables to change
-ExtraLifeID=348774
-textFolder="/home/ermesa/Dropbox/ELtracker/"
-#textFolder="/home/ermesa/Streaming Overlays/donations/"
-CurrencySymbol="$"
 TeamID=None #change to TeamID=None if you aren't in a team
 
 #create URLs
-participant="http://www.extra-life.org/api/participants/"+str(ExtraLifeID)
-donors="http://www.extra-life.org/api/participants/"+str(ExtraLifeID)+"/donations"
 team="http://www.extra-life.org/api/teams/"+str(TeamID)
 
-#api info at https://github.com/DonorDrive/PublicAPI
-
-
-NumberofDonations = 0
-NewNumberofDonations = 0
-
-def writetofile(info,filename):
-    "Handles all the file writes"
-    f = open(textFolder+filename, 'w')
-    f.write(info)
-    f.close
 
 def writetofiletuple(tuple):
     "Handles all the file writes"
     f = open(textFolder+tuple[1], 'w')
     f.write(tuple[0])
     f.close
-
-def ParticipantTopDonor(JSON):
-    TopDonorIndex=0
-    TopDonorNameAmnt=""
-    for donor in range(0,len(JSON)):
-        #need to deal with donations where they hid the donationAmount
-        if JSON[donor]['amount'] == None:
-            print("skipping a null donation amount")
-        elif int(JSON[donor]['amount'])>int(JSON[TopDonorIndex]['amount']):
-            TopDonorIndex=donor
-        TopDonorNameAmnt=str(JSON[TopDonorIndex]['displayName'])+" - "+CurrencySymbol+'{:.2f}'.format(JSON[TopDonorIndex]['amount'])
-    return(TopDonorNameAmnt,"TopDonorNameAmnt.txt")
-
 
 #****** Team Info *******
 
@@ -175,30 +160,7 @@ def TheTeamTotalRaised(JSON):
     TeamTotalRaised=CurrencySymbol+'{:.2f}'.format(JSON['sumDonations'])
     writetofile(TeamTotalRaised,"TeamTotalRaised.txt")
     
-def CountDonors(JSON):
-    return int(JSON['numDonations'])
-
-
 #***** LOOPS ******* 
-
-def ParticipantLoop():
-    try:
-        participantJSON=json.load(urllib.request.urlopen(participant))
-    except urllib.error.HTTPError:
-        print("Couldn't get to participant URL. Check ExtraLifeID. Or server may be unavailable. (participant loop)")
-    try:
-        donorJSON=json.load(urllib.request.urlopen(donors))
-    except urllib.error.HTTPError:
-        print("Couldn't get to donor URL. Check ExtraLifeID. Or server may be unavailable. (participant loop)")
-    writetofiletuple(ParticpantTotalRaised(participantJSON))
-    writetofiletuple(ParticipantGoal(participantJSON))
-    writetofiletuple(ParticipantNumDonations(participantJSON))
-    writetofiletuple(ParticipantAvgDonation(participantJSON))
-    writetofiletuple(ParticipantLastDonorNameAmnt(donorJSON))
-    writetofiletuple(ParticipantTopDonor(donorJSON))
-    writetofiletuple(Participantlast5DonorNameAmts(donorJSON))
-    writetofiletuple(Participantlast5DonorNameAmtsMessage(donorJSON))
-    writetofiletuple(Participantlast5DonorNameAmtsMessageHorizontal(donorJSON))
 
 def TeamLoop():
     if TeamID != None:
@@ -206,36 +168,7 @@ def TeamLoop():
         TheTeamGoal(teamJSON)
         TheTeamTotalRaised(teamJSON)
 
-def main():
-    print ("It's GO TIME!!")
-    print (time.strftime("%H:%M:%S"))
-    ParticipantLoop()
-    TeamLoop()
-    try:
-        participantJSON=json.load(urllib.request.urlopen(participant))
-    except urllib.error.HTTPError:
-        print("Couldn't get to participant URL. Check ExtraLifeID. Or server may be unavailable.")    
-    NumberofDonations = CountDonors(participantJSON)
-    NewNumberofDonations = NumberofDonations
-    
-    while True:
-        print (time.strftime("%H:%M:%S"))
-        try:
-            participantJSON=json.load(urllib.request.urlopen(participant))
-        except urllib.error.HTTPError:
-            print("Couldn't get to participant URL. Check ExtraLifeID. Or server may be unavailable.")
-        NewNumberofDonations = CountDonors(participantJSON)
-        if NewNumberofDonations > NumberofDonations:
-            #for debugging
-            print("We got a new donor!")
-            NumberofDonations = NewNumberofDonations
-            ParticipantLoop()
-            TeamLoop()
-        time.sleep(30)
-
-
 if __name__=="__main__":
 #    main()
     p = Participant()
-    print(p.donorURL)
     p.run()
