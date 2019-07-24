ee1b6f09219c06cc19bde96f3df49f9b81771a40
Eric Mesa <ericsbinaryworld@gmail.com>
2019-07-23 20:16:40 -0400

    just removed commented out self.show in call_tracker to represent that it's no longer needed

--- call_tracker.py
+++ call_tracker.py
@@ -18,7 +18,6 @@ class MyForm(QDialog):
         item=QGraphicsPixmapItem(self.pixmap.scaledToHeight(131))
         self.scene.addItem(item)
         self.ui.graphicsView.setScene(self.scene)
-        #self.show()
         
     @pyqtSlot(bool)    
     def loadElements(self):
