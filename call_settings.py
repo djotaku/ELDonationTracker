d8b4d16c8f4decee81eb073f445994e30b746344
Eric Mesa <ericsbinaryworld@gmail.com>
2019-11-11 22:27:13 -0500

    setting up scaffolding for dealing with version mismatch in the GUI

--- call_settings.py
+++ call_settings.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QInputDialog
 from settings import *
 
 import extralife_IO
@@ -13,7 +13,7 @@ class MyForm(QDialog):
         self.ui.setupUi(self)
         (self.ExtraLifeID, self.textFolder,
          self.CurrencySymbol, self.TeamID, self.TrackerImage,
-         self.DonationSound, self.version) = participant_conf.get_GUI_values()
+         self.DonationSound, self.version_mismatch) = participant_conf.get_GUI_values()
         self.ui.lineEditParticipantID.setText(self.ExtraLifeID)
         self.ui.labelTextFolder.setText(self.textFolder)
         self.ui.lineEditCurrencySymbol.setText(self.CurrencySymbol)
@@ -26,6 +26,20 @@ class MyForm(QDialog):
         self.ui.pushButton_tracker_image.clicked.connect(lambda: self.selectfile("image"))
         self.ui.pushButton_sound.clicked.connect(lambda: self.selectfile("sound"))
         self.show()
+        self.version_check()
+
+    def version_check(self):
+        print("Participant.conf version check!")
+        if self.version_mismatch is True:
+            print("There is a version mismatch")
+            choices = ("Replace with Defaults", "Try to update")
+            choice, ok = QinputDialog.getItem(self, "Input Dialog",
+                                              "Choices", choices, 0,
+                                              False)
+            if ok and choice:
+                print("have to do something with the choice")
+        else:
+            print("Version is correct")
 
     def revert(self):
         self.ui.lineEditParticipantID.setText(self.ExtraLifeID)
