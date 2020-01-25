4d304f56bea088f08ab456aabcaa3766573af2a6
Eric Mesa <ericsbinaryworld@gmail.com>
2020-01-25 10:06:51 -0500

    Documentation and function scope
    Again, added docstrings and made some
    variables private variables. Props to
    Kdevelop for making that incredibly easy
    by showing when functions were being called outside
    the class.

--- call_tracker.py
+++ call_tracker.py
@@ -1,3 +1,5 @@
+"""A window that displays the last donation. Useful during streaming."""
+
 import sys
 from PyQt5.QtWidgets import QDialog, QApplication, QGraphicsScene, QGraphicsPixmapItem
 from tracker import *
@@ -8,7 +10,14 @@ import IPC
 
 
 class MyForm(QDialog):
+    """The class to hold the tracker window."""
+
     def __init__(self, participant_conf):
+        """Set up the window.
+
+        Loads in the image and sound file the user specified in the
+        participant.conf settings file.
+        """
         super().__init__()
         # config stuff
         self.participant_conf = participant_conf
@@ -17,39 +26,46 @@ class MyForm(QDialog):
         self.ui.setupUi(self)
         self.scene = QGraphicsScene(self)
         self.pixmap = QtGui.QPixmap()
-        self.loadimage()
+        self._loadimage()
         self.ui.graphicsView.setScene(self.scene)
         self.donation_player = QMediaPlayer()
-        self.loadsound()
+        self._loadsound()
         # timer to update the main text
         self.timer = QtCore.QTimer(self)
         self.timer.setSingleShot(False)
         self.timer.setInterval(20000)  # milliseconds
-        self.timer.timeout.connect(self.loadAndUnload)
+        self.timer.timeout.connect(self._loadAndUnload)
         self.timer.start()
 
-    def loadimage(self):
+    def _loadimage(self):
         self.tracker_image = self.participant_conf.get_tracker_image()
         self.pixmap.load(self.tracker_image)
         self.item = QGraphicsPixmapItem(self.pixmap.scaledToHeight(131))
 
-    def loadsound(self):
+    def _loadsound(self):
         sound_to_play = self.participant_conf.get_tracker_sound()
         self.donation_sound= QMediaContent(QUrl.fromLocalFile(sound_to_play))
         self.donation_player.setMedia(self.donation_sound)
 
     def loadAndUnloadTest(self):
-        self.loadimage()
-        self.loadElements()
-        self.loadsound()
+        """Trigger the tracker functionality.
+
+        This causes the image and sound to load so that the
+        user can test to see how it's going to look on their OBS
+        or XSplit screen as well as to make sure they can hear
+        the sound. Called by gui.py.
+        """
+        self._loadimage()
+        self._loadElements()
+        self._loadsound()
         self.donation_player.play()
         unloadtimer = QtCore.QTimer(self)
         unloadtimer.setSingleShot(True)
         unloadtimer.setInterval(5000)  # milliseconds
-        unloadtimer.timeout.connect(self.unloadElements)
+        unloadtimer.timeout.connect(self._unloadElements)
         unloadtimer.start()
 
-    def loadAndUnload(self):
+    def _loadAndUnload(self):
         self.folders = self.participant_conf.get_text_folder_only()
         IPC = "0"
         try:
@@ -61,17 +77,17 @@ class MyForm(QDialog):
                 Have you updated the settings?
                 Have you hit the 'run' button?""")
         if IPC == "1":
-            self.loadimage()
-            self.loadElements()
-            self.loadsound()
+            self._loadimage()
+            self._loadElements()
+            self._loadsound()
             self.donation_player.play()
             unloadtimer = QtCore.QTimer(self)
             unloadtimer.setSingleShot(True)
             unloadtimer.setInterval(5000)  # milliseconds
-            unloadtimer.timeout.connect(self.unloadElements)
+            unloadtimer.timeout.connect(self._unloadElements)
             unloadtimer.start()
 
-    def loadElements(self):
+    def _loadElements(self):
         self.scene.addItem(self.item)
         # want to also play a sound
         try:
@@ -82,13 +98,14 @@ class MyForm(QDialog):
         except FileNotFoundError:
             self.ui.Donation_label.setText("TEST 1...2...3...")
 
-    def unloadElements(self):
+    def _unloadElements(self):
         self.scene.removeItem(self.item)
         self.ui.Donation_label.setText("")
         IPC.writeIPC(self.folders, "0")
 
 
 def main(participant_conf):
+    """Run it."""
     w = MyForm(participant_conf)
     w.exec()
 
