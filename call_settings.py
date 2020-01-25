fe955a5a7bf952faf63b9a5389a54c3f8ed4959d
Eric Mesa <ericsbinaryworld@gmail.com>
2020-01-25 09:45:54 -0500

    Documentation and funciton scope
    Mostly added a bunch of docstrings, but also
    made 2 functions private since they're only
    called by this class.

--- call_settings.py
+++ call_settings.py
@@ -1,3 +1,5 @@
+"""Contains the programming logic for the settings window in the GUI."""
+
 import sys
 import json
 from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
@@ -7,7 +9,15 @@ import extralife_IO
 
 
 class MyForm(QDialog):
+    """Class for the settings Window."""
+
     def __init__(self, participant_conf):
+        """Init for the settings Window.
+
+        Grabs the data from the participant.conf file and
+        uses that to pre-populate the fields in the settings
+        window.
+        """
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
@@ -25,21 +35,31 @@ class MyForm(QDialog):
         self.ui.pushButtonRevert.clicked.connect(self.revert)
         self.ui.pushButtonSave.clicked.connect(self.save)
         self.ui.pushButton_persistentsave.clicked.connect(self.persistent_save)
-        self.ui.pushButtonSelectFolder.clicked.connect(self.selectfolder)
-        self.ui.pushButton_tracker_image.clicked.connect(lambda: self.selectfile("image"))
-        self.ui.pushButton_sound.clicked.connect(lambda: self.selectfile("sound"))
-        #self.ui.spinBox_DonorsToDisplay.valueChanged.connect(self.donorschanged)
+        self.ui.pushButtonSelectFolder.clicked.connect(self._selectfolder)
+        self.ui.pushButton_tracker_image.clicked.connect(lambda: self._selectfile("image"))
+        self.ui.pushButton_sound.clicked.connect(lambda: self._selectfile("sound"))
+        # self.ui.spinBox_DonorsToDisplay.valueChanged.connect(self.donorschanged)
         if self.donors_to_display is None:
             self.ui.spinBox_DonorsToDisplay.setValue(0)
         else:
             self.ui.spinBox_DonorsToDisplay.setValue(int(self.donors_to_display))
 
     def reload_config(self):
+        """Reload the values from the config file.
+
+        Called by gui.py before loading the settings window.
+        """
         (self.ExtraLifeID, self.textFolder,
          self.CurrencySymbol, self.TeamID, self.TrackerImage,
          self.DonationSound, self.donors_to_display) = self.participant_conf.get_GUI_values()
 
     def revert(self):
+        """
+        Revert the values in the settings window.
+
+        If the user made mistakes while editing the config this will
+        take them back to the values since they opened the window.
+        """
         self.ui.lineEditParticipantID.setText(self.ExtraLifeID)
         self.ui.labelTextFolder.setText(self.textFolder)
         self.ui.lineEditCurrencySymbol.setText(self.CurrencySymbol)
@@ -72,18 +92,23 @@ class MyForm(QDialog):
         return config
 
     def save(self):
+        """Save the values in the window.
+
+        Calls the write_config method from extralife_IO.ParticipantConf
+        """
         config = self._elements_to_save()
         self.participant_conf.write_config(config, True)
 
     def persistent_save(self):
+        """Use xdg_config, saves a persistent config to the XDG spot."""
         config = self._elements_to_save()
         self.participant_conf.write_config(config, False)
 
-    def selectfolder(self):
+    def _selectfolder(self):
         directory = QFileDialog.getExistingDirectory(self, "Get Folder")
         self.ui.labelTextFolder.setText(directory)
 
-    def selectfile(self, whichfile):
+    def _selectfile(self, whichfile):
         the_file = QFileDialog.getOpenFileName(self, "Select File")
         if whichfile == "image":
             self.ui.label_tracker_image.setText(the_file[0])
@@ -92,6 +117,7 @@ class MyForm(QDialog):
 
 
 def main(participant_conf):
+    """Launch it."""
     w = MyForm(participant_conf)
     w.exec()
 
