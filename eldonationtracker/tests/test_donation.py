a513fbe441ba6bb7984fc23fe7543abb1c2fda7e
Eric Mesa <ericsbinaryworld@gmail.com>
2020-04-10 00:17:44 -0400

    Added __str__ to donation.py and unit tests

--- eldonationtracker/tests/test_donation.py
+++ eldonationtracker/tests/test_donation.py
@@ -1,39 +1,63 @@
 """Test methods in donor class."""
 
-from eldonationtracker import donor
+from eldonationtracker import donation
 
 
-def test_donor_lt_whole_numbers():
+def test_donation_attributes_no_blanks():
+    """Test to make sure attributes are properly assigned."""
+    donation1 = donation.Donation("Donor 1", "Good job!", 34.51)
+    assert donation1.name == "Donor 1"
+    assert donation1.message == "Good job!"
+    assert donation1.amount == 34.51
+
+
+def test_donation_attributes_no_name():
+    """Test to make sure attributes are properly assigned."""
+    donation1 = donation.Donation(None, "Good job!", 34.51)
+    assert donation1.name == "Anonymous"
+    assert donation1.message == "Good job!"
+    assert donation1.amount == 34.51
+
+
+def test_donation_attributes_no_amount():
+    """Test to make sure attributes are properly assigned."""
+    donation1 = donation.Donation("Donor 1", "Good job!", None)
+    assert donation1.name == "Donor 1"
+    assert donation1.message == "Good job!"
+    assert donation1.amount == 0
+
+
+def test_donation_lt_whole_numbers():
     """Test to make sure comparison works.
 
     Uses whole dollar amounts.
     """
-    donor1_json = {"displayName": "donor1", "sumDonations": "45"}
-    donor2_json = {"displayName": "donor2", "sumDonations": "30"}
-    donor1 = donor.Donor(donor1_json)
-    donor2 = donor.Donor(donor2_json)
-    assert donor2 < donor1
+    donation1 = donation.Donation("Donor 1", "Good job!", 34)
+    donation2 = donation.Donation("Donor 1", "Good job!", 32)
+    assert donation2 < donation1
 
 
-def test_donor_lt_change():
+def test_donation_lt_change():
     """Test to make sure comparison works.
 
     Uses dollars and cents.
     """
-    donor1_json = {"displayName": "donor1", "sumDonations": "30.00"}
-    donor2_json = {"displayName": "donor2", "sumDonations": "30.01"}
-    donor1 = donor.Donor(donor1_json)
-    donor2 = donor.Donor(donor2_json)
-    assert donor1 < donor2
+    donation1 = donation.Donation("Donor 1", "Good job!", 34.51)
+    donation2 = donation.Donation("Donor 1", "Good job!", 34.50)
+    assert donation2 < donation1
 
 
-def test_donors_equal():
+def test_donation_equal():
     """Test to make sure comparison works.
 
     This time they're equal.
     """
-    donor1_json = {"displayName": "donor1", "sumDonations": "30"}
-    donor2_json = {"displayName": "donor2", "sumDonations": "30"}
-    donor1 = donor.Donor(donor1_json)
-    donor2 = donor.Donor(donor2_json)
-    assert donor1 == donor2
+    donation1 = donation.Donation("Donor 1", "Good job!", 34.51)
+    donation2 = donation.Donation("Donor 1", "Good job!", 34.51)
+    assert donation2 == donation1
+
+
+def test_donation_str():
+    """Test that the class string is properly created."""
+    donation1 = donation.Donation("Donor 1", "Good job!", 34.51)
+    assert str(donation1) == "A donation by Donor 1 in the amount of $34.51 with the message 'Good job!'"
