bd1b765a4e839fb093677a22f53a9c2ff51b45cc
Eric Mesa <ericsbinaryworld@gmail.com>
2020-06-04 20:43:33 -0400

    Added a test for when the donor is Anonymous.

--- eldonationtracker/tests/test_team_participant.py
+++ eldonationtracker/tests/test_team_participant.py
@@ -8,3 +8,10 @@ def test_team_participant_str():
                         'avatarImageURL': "http://someplace.com/image.jpg", "numDonations": 2}
     team_donor1 = eldonationtracker.team_participant.TeamParticipant(team_donor1_json)
     assert str(team_donor1) == "A Team Participant named donor1 who has donated $45.00 to the team over 2 donations."
+
+
+def test_team_participant_str_no_donor_name():
+    team_donor1_json = {"sumDonations": "45", "donorID": 1000111,
+                        'avatarImageURL': "http://someplace.com/image.jpg", "numDonations": 2}
+    team_donor1 = eldonationtracker.team_participant.TeamParticipant(team_donor1_json)
+    assert str(team_donor1) == "A Team Participant named Anonymous who has donated $45.00 to the team over 2 donations."
