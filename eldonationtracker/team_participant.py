f6ec710eab0d65aadafc5c909ad8b3fcdc443348
Eric Mesa <ericsbinaryworld@gmail.com>
2021-01-13 19:29:16 -0500

    Removed redundant code from team_participant.py
    while reading Clean Code, I realized I had redundant code that was abusing inheritance. Works much better now.

--- eldonationtracker/team_participant.py
+++ eldonationtracker/team_participant.py
@@ -15,23 +15,5 @@ class TeamParticipant(donor.Donor):
     :param self.image_url: the url of the participant's avatar image (not used)
     """
 
-    def __init__(self, json):
-        self.name, self.amount, self.number_of_donations, self.image_url = self.json_to_attributes(json)
-
-    def json_to_attributes(self, json):
-        """Convert JSON to Team Participant attributes.
-
-        :param json: JSON attributes from API
-        """
-        name = ""
-        if json.get('displayName') is not None:
-            name = json.get('displayName')
-        else:
-            name = "Anonymous"
-        amount = float(json.get("sumDonations"))
-        number_of_donations = json.get('numDonations')
-        image_url = json.get('avatarImageURL')
-        return name, amount, number_of_donations, image_url
-
     def __str__(self):
         return f"A Team Participant named {self.name} who has donated ${self.amount:.2f} to the team over {self.number_of_donations} donations."
